d5ecc118e0ddd53e293898b93fc13cc3a4387410
lekan <laksidesvibe@gmail.com>
2023-04-02 21:35:16 -0400

    complete

--- index.js
+++ index.js
@@ -13,4 +13,10 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials
+  .map(tutorial => { // map over each tutorial
+    let words = tutorial.split(' ') // split each tutorial into an array of words
+    let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)) // do a map over spec. word in array and uppercase 1st letter
+    let response = capitalizedWords.join(' ') // join the array of capitalized words back into a string
+    return response // return the string
+  })
 }
